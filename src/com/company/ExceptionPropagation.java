393e5012a3bc8492793c1e0537ffbc8d53e3dfa9
harihararaj <harish.jaybal@gmail.com>
2021-04-09 13:04:41 +0530

    Exceptions

--- src/com/company/ExceptionPropagation.java
+++ src/com/company/ExceptionPropagation.java
@@ -1,5 +1,20 @@
 package com.company;
-
 public class ExceptionPropagation {
-
+        public static void main(String[] args){
+            try{
+                m1();
+            }
+            catch (ArithmeticException e){
+               System.out.println("There is a Exception in m3()");
+            }
+        }
+        static void m1(){
+            m2();
+        }
+        static void m2(){
+            m3();
+        }
+        static void m3(){
+            System.out.println(10/0);
+        }
 }
